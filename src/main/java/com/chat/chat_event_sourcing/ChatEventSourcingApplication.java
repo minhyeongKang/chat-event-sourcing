8f377a851ffacfadb93680b8ead66a4c3467bbd7
minhyeongKang <kang7937@gmail.com>
2026-05-18 11:35:34 +0900

    feat: init Spring Boot project

---
+++ src/main/java/com/chat/chat_event_sourcing/ChatEventSourcingApplication.java
@@ -0,0 +1,13 @@
+package com.chat.chat_event_sourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatEventSourcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatEventSourcingApplication.class, args);
+	}
+
+}
